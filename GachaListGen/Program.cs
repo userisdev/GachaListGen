852b93cbae7af49d5200d6c0e382da9568972246
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-18 12:41:23 +0900

    fisrt commit.

---
+++ GachaListGen/Program.cs
@@ -0,0 +1,27 @@
+﻿var srcDirPath = args.FirstOrDefault();
+if (!Directory.Exists(srcDirPath))
+{
+    Console.WriteLine("not found.");
+    Environment.Exit(1);
+}
+
+var key = args.ElementAtOrDefault(1);
+if (string.IsNullOrWhiteSpace(key))
+{
+    Console.WriteLine($"invalid key. [{key}]");
+    Environment.Exit(1);
+}
+
+var baseUrl  = args.ElementAtOrDefault(2);
+if (string.IsNullOrWhiteSpace(baseUrl))
+{
+    Console.WriteLine($"invalid base url. [{baseUrl}]");
+    Environment.Exit(1);
+}
+
+var savePath = Path.Combine(Environment.CurrentDirectory, "list.csv");
+var srcDir = new DirectoryInfo(srcDirPath);
+var files = srcDir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+var lines = files.Select(e => string.Join(",",key,Path.GetFileNameWithoutExtension(e.Name).Replace("_"," "),baseUrl + e.Name));
+File.WriteAllLines(savePath, lines);
+Environment.Exit(0);
